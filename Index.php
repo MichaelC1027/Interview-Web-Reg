4058090a21318799cb0bb23ad7b4d33ee44f3a03
MichaelC1027 <macastillo1027@gmail.com>
2022-03-15 17:42:11 -0500

    The Rework
    Adding all the files needed to have this working and running perfectly! this is the 1.0 of the current project

--- Index.php
+++ Index.php
@@ -24,40 +24,30 @@ Insert all form data into DB and return “Success”
 -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="global.css" />
     <title>Registration Website</title>
-    
+
     <!--The Link to the Bootstrap page-->
-    <link 
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" 
-    crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <!--The Bootstrap insert script -->
-    <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" 
-    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
     </script>
 
     <!--The J Query insert script-->
-    <script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
     </script>
 
     <!--THe J Query code section-->
     <script>
         // Jquery Code in here to help with validation
-        $(document).ready(function()
-        {
-            $("form").submit(function(event)
-            {
+        $(document).ready(function() {
+            $("form").submit(function(event) {
                 event.preventDefault();
                 var firstName = $("#firstName").val();
                 var lastName = $("#lastName").val();
@@ -66,10 +56,9 @@ Insert all form data into DB and return “Success”
                 var optin = $("#optin").val();
                 var g_recaptcha = $("#g-recaptcha-response").val();
                 var submit = $("#submit").val();
-                
 
-                $(".form-message").load("Php_Backend.php", 
-                {
+
+                $(".form-message").load("Php_Backend.php", {
                     //the first variable is the actual post variable, the variable after is the actual value we have obtained
                     firstName: firstName,
                     lastName: lastName,
@@ -81,56 +70,58 @@ Insert all form data into DB and return “Success”
                 });
             });
         });
-
     </script>
 
     <!--The Google recaptcha insert script-->
     <script src="https://www.google.com/recaptcha/api.js"></script>
 
 </head>
+
 <body>
 
     <div class="container">
-        <div class="col-sm-6">
-            <form action="Php_Backend.php" method="post">
-                
-                <div class="form-group">
-                    <label for="firstname"> First Name </label>
-                    <input type="text" name="firstName" placeholder = "First Name" class="form-control" id="firstName">
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="lastname"> Last Name </label>
-                    <input type="text" name="lastName" placeholder="Last Name" class="form-control" id="lastName">
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="email"> Email Address </label>
-                    <input type="email" name="email" placeholder="Email Address" class="form-control" id="email">
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="confirm"> Confirm Email Address </label>
-                    <input type="email" name="confirm_email" placeholder="Confirmation" class="form-control" id="confirm_email">
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="optin"> Would you like to Opt-in for our other offers? </label>
-                    <input type="checkbox" name="optin" value="Yes" id="optin">
-                </div>
-                <br>
-                <div class = "form-group">
-                    <div class="g-recaptcha" data-sitekey = "6LfqD7AcAAAAAODVLqjGOkXyJJvEIrVy-u6s4RC0" id="g-recaptcha-response"></div>
-                </div>
-                <br>
-                <input type="submit" value="Register" name="submit" class="btn btn-primary" id="submit">
-                <br>
-                <div class="form-group">   
-                    <p class="form-message"></p>
-                </div>
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <form action="Php_Backend.php" method="post" class="form-box">
 
-            </form>
+                    <div class="form-group">
+                        <label for="firstname"> First Name </label>
+                        <input type="text" name="firstName" placeholder="First Name" class="form-control" id="firstName">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="lastname"> Last Name </label>
+                        <input type="text" name="lastName" placeholder="Last Name" class="form-control" id="lastName">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="email"> Email Address </label>
+                        <input type="email" name="email" placeholder="Email Address" class="form-control" id="email">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="confirm"> Confirm Email Address </label>
+                        <input type="email" name="confirm_email" placeholder="Confirmation" class="form-control" id="confirm_email">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="optin"> Would you like to Opt-in for our other offers? </label>
+                        <input type="checkbox" name="optin" value="Yes" id="optin">
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <div class="g-recaptcha" data-sitekey="6LfqD7AcAAAAAODVLqjGOkXyJJvEIrVy-u6s4RC0" id="g-recaptcha-response"></div>
+                    </div>
+                    <br>
+                    <input type="submit" value="Register" name="submit" class="btn btn-primary" id="submit">
+                    <br>
+                    <div class="form-group">
+                        <p class="form-message"></p>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
--- Php_Backend.php
+++ Php_Backend.php
@@ -23,159 +23,112 @@ Insert all form data into DB and return “Success” (DONE)
 
 -->
 
-<?php 
+<?php
+require 'vendor/autoload.php';
 
-    include"connection.php";
-
-if(isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
 
     //here we are getting the values from the POST that the user has submitted
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $email_confirm = $_POST['confirm_email'];
-    
-    
+
+
     //date checker and errors
     $errorEmpty = false;
     $errorEmail = false;
     $emailDifferent = false;
-    $date = date('Y-m-d'); 
+    $date = date('Y-m-d');
 
     //here we are checking if the optin option was yes or no and setting its value
-    if(isset($_POST['optin']))
-    {
+    if (isset($_POST['optin'])) {
         $optIn = $_POST['optin'];
-    }
-    else
-    {
+    } else {
         $optIn = "no";
     }
 
     //here we are checking if any of the inputs are empty and returning a echo of an empty field
-    if(empty($firstName) || empty($lastName) || empty($email) || empty($email_confirm))
-    {
+    if (empty($firstName) || empty($lastName) || empty($email) || empty($email_confirm)) {
         echo "<span> Fill in all the fields please! (Validation error) </span>" . "<br>";
         $errorEmpty = true;
-    }
-    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL) || !filter_var($email_confirm, FILTER_VALIDATE_EMAIL))
-    {
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) || !filter_var($email_confirm, FILTER_VALIDATE_EMAIL)) {
         echo "<span> Please submit a valid e-mail address! (Validation error) </span>" . "<br>";
         $errorEmail = true;
-    }
-    elseif ($email != $email_confirm) 
-    {
+    } elseif ($email != $email_confirm) {
 
         echo "<span> Emails were incorrect from each other (Validation error) </span>" . "<br>";
         $emailDifferent = true;
-    }
-    else
-    {
+    } else {
         // reCAPTCHA validation
-        if(isset($_POST['g_recaptcha']) && !empty($_POST['g_recaptcha'])) {
+        if (isset($_POST['g_recaptcha']) && !empty($_POST['g_recaptcha'])) {
 
             // Google secret API
             $secretAPIkey = '6LfqD7AcAAAAADG1Hie7I2EFrXZ3cgNw-quqPNDP';
 
             // reCAPTCHA response verification
-            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretAPIkey.'&response='.$_POST['g_recaptcha']);      
-                
-                //here we are decoding the verification response
-                $response = json_decode($verifyResponse);
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretAPIkey . '&response=' . $_POST['g_recaptcha']);
 
-                //if the response was successful we will run this code
-                if($response->success){
-                    //we are using a try catch block incase any errors come up
-                    try {
-                        //this variable connects to the database
-                        //$connection = mysqli_connect('localhost','root','','interview');
-                
-                        //if the connection is good run code below
-                        if($connection == true)
-                        {
-                            //this code block below is checking of if we have a email in the database already
-                            //if so, we check the date and make sure they are not submitting more then once a day
-                            $query = "SELECT TodaysDate FROM customers WHERE Email = '$email'";
-                            $result = mysqli_query($connection,$query);
-                            $TodaysDateCheck = "";
-                            $datesAreEqual = false;
-                            
-                            //here we are obtaining an associative array from the database
-                            while($row = mysqli_fetch_assoc($result))
-                            {
-                                global $TodaysDateCheck;
-                                //this is turning the array information into a string
-                                $TodaysDateCheck = implode("",$row);
-                            }
-                
-                            //here we are checking the date to see if they match 
-                            if($TodaysDateCheck == $date)
-                            {
-                                global $datesAreEqual;
-                                $datesAreEqual = true;
-                            }
-                
-                
-                            //if the dates do not match we run the code down below, otherwise we give an error
-                            if ($datesAreEqual != true) 
-                            {
-                                //here we are taking the info that was submitted and putting it into an SQL query format
-                                $query = "INSERT INTO customers(FirstName,LastName,Email,Offers,TodaysDate) VALUES ('$firstName','$lastName','$email','$optIn','$date')";
-                
-                                //here we are using the mysqli_query to have the query string we just made run on the connection to the database
-                                $result = mysqli_query($connection,$query);
-                
-                                //here the result is false(it didnt make a new entry) then it will kill the program and tell us we failed
-                                if($result != true)
-                                {
-                                    die("FAILED TO INPUT");
-                                }
-                                else
-                                {
-                                    echo "Thank you for Registering, we will be emailing you soon! (Success)";
-                                }
-                            }
-                            else
-                            {
-                                die("You have already submitted for today! Please try again Tomorrow (Limit Error)");
-                            }
-                
-                
-                        }
-                
-                    } 
-                    catch (\Throwable $th) 
-                    {
-                        //throw $th;
-                        die("Database connection failed");
-                    }
-                
+            //here we are decoding the verification response
+            $response = json_decode($verifyResponse);
+
+            //if the response was successful we will run this code
+            if ($response->success) {
+                //we are using a try catch block incase any errors come up
+                try {
+                    //create instance of DynamoDB client
+                    $client = Aws\DynamoDb\DynamoDbClient::factory(array(
+                        'region' => 'us-east-1',
+                        'version' => 'latest',
+                        'credentials' => array(
+                            'key' => 'AKIAQIL6GLPF6Y3DC6G3',
+                            'secret'  => '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',
+                        )
+                    ));
+
+                    //generate a new id
+                    $new_id = (string)(mt_rand(1, 1000));
+                    $id = $new_id;
+
+                    //create new item to add to table
+                    $marshaler = new Aws\DynamoDb\Marshaler();
+                    $item = $marshaler->marshalItem(
+                        [
+                            'id' => $id,
+                            'first_name' => $firstName,
+                            'last_name' => $lastName,
+                            'email' => $email,
+                            'email_confirm' => $email_confirm,
+                            'optIn' => $optIn
+                        ]
+                    );
+
+                    $params = [
+                        'TableName' => 'Customers',
+                        'Item' => $item
+                    ];
+
+                    $result = $client->putItem($params);
+                    
+                } catch (\AWS\DynamoDb\Exception\DynamoDbException $e) {
+                    echo "Unable to add item:\n";
+                    echo $e->getMessage() . "\n";
+                }
             }
-
-        }
-        else
-        {
+        } else {
             echo "please verify you are a human and not a robot with the recaptcha!";
         }
     }
-    
-    
-
-    
 }
-    
-    ?>
 
-    <script>
+?>
 
-        var errorEmpty = "<?php echo $errorEmpty; ?>";
-        var errorEmail = "<?php echo $errorEmail; ?>";
-        var emailDif = "<?php echo $emailDifferent; ?>"
-        
-        if(errorEmpty == false && errorEmail == false && emailDif == false)
-        {
-            $("#firstName, #lastName, #email, #confirm_email").val("");
-        }
+<script>
+    var errorEmpty = "<?php echo $errorEmpty; ?>";
+    var errorEmail = "<?php echo $errorEmail; ?>";
+    var emailDif = "<?php echo $emailDifferent; ?>"
 
-    </script>
+    if (errorEmpty == false && errorEmail == false && emailDif == false) {
+        $("#firstName, #lastName, #email, #confirm_email").val("");
+    }
+</script>
